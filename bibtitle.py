47bd38c7fb51fcd3d54f6cc7cf26783cc87846c4
Manos Kav <manoskav@users.noreply.github.com>
2020-05-28 23:59:46 +0300

    Add files via upload

---
+++ bibtitle.py
@@ -0,0 +1,52 @@
+import os, sys, re
+
+bibname = sys.argv[1]
+assert os.path.exists(bibname), "File does not exist."
+assert bibname[-3:] == "bib", "Bibtex file must have .bib extension."
+
+with open(bibname, 'r') as file:
+    lines = file.readlines()
+
+#  Regular expression to catch the "title" bibtex tag
+pattern = r'(^(\s)*)(title)(\s)*(=)(\s)*{(?P<title>.*)}(\s)*(,?)((\s)*$)'
+regex = re.compile(pattern, flags=re.IGNORECASE)
+
+for i in range(len(lines)):
+
+    line = lines[i]
+    
+    hastitle = regex.search(line)
+
+    if hastitle is not None:
+
+        tmptitle = hastitle.group('title')
+
+
+        lentitle = len(tmptitle)
+        j = 0
+        while j < lentitle - 1:
+            char = tmptitle[j]
+            if j > 0:
+                prechar = tmptitle[j-1]
+            else:
+                prechar = ''
+
+            if j < lentitle-1:
+                postchar = tmptitle[j+1]
+            else:
+                postchar = ''
+                
+            if char.isupper():
+                if not (prechar == "{" and postchar == "}"):
+                    tmptitle = "".join(
+                            [tmptitle[:j], "{"+char+"}", tmptitle[j+1:]])
+                    j += 2
+
+            j += 1
+            lentitle = len(tmptitle)
+
+        start, end = hastitle.span('title')
+        lines[i] = "".join([line[0:start], tmptitle, line[end:]])
+
+with open(bibname[:-4]+"-new"+".bib", 'w') as file:
+    file.writelines(lines)
