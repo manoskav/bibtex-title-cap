719dd74d669d539bef8130b84b6a81edfed9fb8e
Manos Kav <manoskav@users.noreply.github.com>
2020-06-01 20:22:34 +0300

    Regular Expression update
    Updated regular expression to capture tags enclosed both by {...} and "...".

--- bibtitle.py
+++ bibtitle.py
@@ -8,7 +8,7 @@ with open(bibname, 'r') as file:
     lines = file.readlines()
 
 #  Regular expression to catch the "title" bibtex tag
-pattern = r'(^(\s)*)(title)(\s)*(=)(\s)*{(?P<title>.*)}(\s)*(,?)((\s)*$)'
+pattern = r'(^(\s)*)(title)(\s)*(=)(\s)*({|")(?P<title>.*)(}|")(\s)*(,?)((\s)*$)'
 regex = re.compile(pattern, flags=re.IGNORECASE)
 
 for i in range(len(lines)):
@@ -29,23 +29,18 @@ for i in range(len(lines)):
             char = tmptitle[j]
                 
             if char.isupper():
-                if j > 0:
-                    prechar = tmptitle[j-1]
-                else:
-                    prechar = ''
-
-                if j < lentitle-1:
-                    postchar = tmptitle[j+1]
-                else:
-                    postchar = ''
+                
+                prechar = tmptitle[j-1] if j>0 else ''
+                postchar = tmptitle[j+1] if j<lentitle-1 else ''
 
                 if not (prechar == "{" and postchar == "}"):
                     tmptitle = "".join(
                             [tmptitle[:j], "{"+char+"}", tmptitle[j+1:]])
                     j += 2
-
+                    lentitle = len(tmptitle)
+                    
             j += 1
-            lentitle = len(tmptitle)
+            
 
         start, end = hastitle.span('title')
         lines[i] = "".join([line[0:start], tmptitle, line[end:]])
